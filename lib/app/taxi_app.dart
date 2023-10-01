c93b79d6f4b475f584d341adeed53d23f418652f
Amir55143 <aamirnoor55143@gmail.com>
2023-10-01 12:24:40 +0500

    Taxi App

---
+++ lib/app/taxi_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TaxiApp extends StatelessWidget {
+  const TaxiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
